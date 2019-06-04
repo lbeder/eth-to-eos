c273b1136f66d9ea54b58dbe0da59c708f3e3422
Leonid Beder <leonid.beder@gmail.com>
2019-06-04 14:14:15 +0300

    Add initial project

---
+++ index.js
@@ -0,0 +1,29 @@
+const ecc = require('eosjs-ecc');
+const eth = require('ethereumjs-util');
+const prompt = require('password-prompt');
+
+const convertKey = async () => {
+  const inputPrivateKey = await prompt('ETH Private Key: ', { method: 'hide' });
+  const rawPrivateKey = inputPrivateKey.startsWith('0x') ? inputPrivateKey.substr(2) : inputPrivateKey;
+  const ethPrivateKey = Buffer.from(rawPrivateKey, 'hex');
+
+  if (!eth.isValidPrivate(ethPrivateKey)) {
+    console.error('Invalid Ethereum Private Key!');
+    return;
+  }
+
+  const ethAddress = `0x${eth.privateToAddress(ethPrivateKey).toString('hex')}`;
+  const ethPublicKey = eth.privateToPublic(ethPrivateKey).toString('hex');
+
+  console.log(`ETH Address: ${ethAddress}`);
+  console.log(`ETH Public Key: ${ethPublicKey}`);
+
+  const eosWIF = ecc.PrivateKey(ethPrivateKey).toWif();
+  const eosPrivateKey = eosWIF;
+  const eosPublicKey = ecc.privateToPublic(eosWIF);
+
+  console.log(`EOS Private Key: ${eosPrivateKey}`);
+  console.log(`EOS Public Key: ${eosPublicKey}`);
+};
+
+convertKey();
